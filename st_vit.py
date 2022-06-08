340dc6710e6a2c70914badd9c5640ced751c889b
Nicolai Lassen <mail@nmtl.dk>
2022-06-08 03:07:42 +0200

    cc

--- st_vit.py
+++ st_vit.py
@@ -1,175 +1,137 @@
 import torch.nn as nn
 import torch
 
-from functools import wraps
 from einops import rearrange
 from einops.layers.torch import Rearrange
 from rotary_embedding_torch import RotaryEmbedding
 
 # helpers
 
-def _many(fn):
-    @wraps(fn)
-    def inner(tensors, pattern, **kwargs):
-        return (fn(tensor, pattern, **kwargs) for tensor in tensors)
-    return inner
+def INF(B,H,W):
+     return -torch.diag(torch.tensor(float("inf")).cuda().repeat(H),0).unsqueeze(0).repeat(B*W,1,1)
 
-rearrange_many = _many(rearrange)
+class CrissCrossAttention(nn.Module):
+    """ Criss-Cross Attention Module"""
+    
+    def __init__(self, pos, in_dim):
+        super(CrissCrossAttention,self).__init__()
+        
+        self.pos = pos
+        self.query_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim//8, kernel_size=1)
+        self.key_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim//8, kernel_size=1)
+        self.value_conv = nn.Conv2d(in_channels=in_dim, out_channels=in_dim, kernel_size=1)
+
+        self.softmax = nn.Softmax(dim=3)
+        
+        self.INF = INF
+        self.gamma = nn.Parameter(torch.zeros(1))
+
+    def forward(self, x):
+        m_batchsize, _, height, width = x.size()
+        
+        proj_query = self.query_conv(x)
+        proj_query = self.pos.rotate_queries_or_keys(proj_query)
+        
+        proj_query_H = proj_query.permute(0,3,1,2).contiguous().view(m_batchsize*width,-1,height).permute(0, 2, 1)
+        proj_query_W = proj_query.permute(0,2,1,3).contiguous().view(m_batchsize*height,-1,width).permute(0, 2, 1)
+        
+        proj_key = self.key_conv(x)
+        proj_key = self.pos.rotate_queries_or_keys(proj_key)
+        
+        proj_key_H = proj_key.permute(0,3,1,2).contiguous().view(m_batchsize*width,-1,height)
+        proj_key_W = proj_key.permute(0,2,1,3).contiguous().view(m_batchsize*height,-1,width)
+        
+        proj_value = self.value_conv(x)
+        proj_value_H = proj_value.permute(0,3,1,2).contiguous().view(m_batchsize*width,-1,height)
+        proj_value_W = proj_value.permute(0,2,1,3).contiguous().view(m_batchsize*height,-1,width)
+        
+        energy_H = (torch.bmm(proj_query_H, proj_key_H)+self.INF(m_batchsize, height, width)).view(m_batchsize,width,height,height).permute(0,2,1,3)
+        energy_W = torch.bmm(proj_query_W, proj_key_W).view(m_batchsize,height,width,width)
+        
+        concate = self.softmax(torch.cat([energy_H, energy_W], 3))
+
+        att_H = concate[:,:,:,0:height].permute(0,2,1,3).contiguous().view(m_batchsize*width,height,height)
+
+        att_W = concate[:,:,:,height:height+width].contiguous().view(m_batchsize*height,width,width)
+        out_H = torch.bmm(proj_value_H, att_H.permute(0, 2, 1)).view(m_batchsize,width,-1,height).permute(0,2,3,1)
+        out_W = torch.bmm(proj_value_W, att_W.permute(0, 2, 1)).view(m_batchsize,height,-1,width).permute(0,2,1,3)
+
+        return self.gamma*(out_H + out_W) + x
 
 # classes
 
-class PreNorm(nn.Module):
-    def __init__(self, dim, fn):
-        super().__init__()
-        self.norm = nn.LayerNorm(dim)
-        self.fn = fn
-    def forward(self, x, **kwargs):
-        return self.fn(self.norm(x), **kwargs)
-
-class FeedForward(nn.Module):
-    def __init__(self, dim, hidden_dim, dropout = 0.):
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.Linear(dim, hidden_dim),
-            nn.GELU(),
-            nn.Dropout(dropout),
-            nn.Linear(hidden_dim, dim),
-            nn.Dropout(dropout)
-        )
-    def forward(self, x):
-        return self.net(x)
-
-class Attention(nn.Module):
-    def __init__(self, pos, dim, heads = 8, dim_head = 64, dropout = 0.):
-        super().__init__()
-        inner_dim = dim_head *  heads
-        project_out = not (heads == 1 and dim_head == dim)
-
-        self.pos = pos
-
-        self.heads = heads
-        self.scale = dim_head ** -0.5
-        
-        self.attend = nn.Softmax(dim = -1)
-        self.dropout = nn.Dropout(dropout)
-
-        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
-
-        self.to_out = nn.Sequential(
-            nn.Linear(inner_dim, dim),
-            nn.Dropout(dropout)
-        ) if project_out else nn.Identity()
-
-    def forward(self, x):
-        q, k, v = self.to_qkv(x).chunk(3, dim = -1)
-        q, k, v = rearrange_many((q, k, v), 'b n (h d) -> b h n d', h = self.heads)
-
-        q = self.pos.rotate_queries_or_keys(q)
-        k = self.pos.rotate_queries_or_keys(k)
-
-        dots = torch.matmul(q, k.transpose(-1, -2)) * self.scale
-
-        attn = self.attend(dots)
-        attn = self.dropout(attn)
-
-        out = torch.matmul(attn, v)
-        out = rearrange(out, 'b h n d -> b n (h d)')
-        return self.to_out(out)
-
-class SpatioTemporalTransformer(nn.Module):
-    def __init__(self, dim, depth, heads, dim_head, dropout = 0.):
-        super().__init__()
-        
-        # https://arxiv.org/abs/2104.09864
-        pos = RotaryEmbedding(dim = dim_head)
-        
-        self.norm = nn.LayerNorm(dim)
-        
-        self.layers = nn.ModuleList([])
-        for _ in range(depth):
-            self.layers.append(nn.ModuleList([
-                PreNorm(dim, Attention(pos, dim, heads = heads, dim_head = dim_head, dropout = dropout)),
-                PreNorm(dim, Attention(pos, dim, heads = heads, dim_head = dim_head, dropout = dropout)),
-                nn.LayerNorm(dim)
-            ]))
-             
-    def forward(self, x):
-        b = x.shape[0]
-       
-        # https://arxiv.org/abs/2205.15868
-        for attn_s, attn_t, norm in self.layers:
-            
-            x_s = rearrange(x, 'b t p d -> (b t) p d')
-            x_s = rearrange(attn_s(x_s), '(b t) p d -> b t p d', b=b)
-            
-            x_t = rearrange(x + x_s, 'b t p d -> (b p) t d')    
-            x_t = rearrange(attn_t(x_t), '(b p) t d -> b t p d', b=b)
-            
-            x = norm(x_s + x_t) + x
-            
-        return self.norm(x)
-
-# https://arxiv.org/abs/2010.11929
-class TVit(nn.Module):
-    def __init__(self, *, dim, depth, heads, dim_head = 64, dropout = 0., emb_dropout = 0.):
-        super().__init__()
-        self.dropout = nn.Dropout(emb_dropout)
-        self.transformer = SpatioTemporalTransformer(dim, depth, heads, dim_head, dropout)
-        self.mlp = nn.Linear(dim, dim)
-        
-    def forward(self, x):
-        x = self.dropout(x)
-        x = self.transformer(x)
-        return self.mlp(x)
-
-# https://arxiv.org/abs/2010.11929
-class STAViT(nn.Module):
+class TAViT(nn.Module):
     def __init__(self,
+                 max_seq_len=15,
                  channels=1, 
                  patch_size=64,
-                 dim=256, 
-                 heads=4,
-                 depth=2,
-                 dim_head=64
+                 dim=128,
+                 recurrence=3,
+                 dim_head=64,
+                 dropout=0.01,
+                 emb_dropout=0.1,
                  ):
         super().__init__()
         
+        self.max_seq_len = max_seq_len        
         pixel_values_per_patch = channels * patch_size * patch_size
         
-        self.to_patch_embedding = nn.Sequential(
+        self.to_embedding = nn.Sequential(
             Rearrange('b t c (h p1) (w p2) -> b t (h w) (c p1 p2)', p1 = patch_size, p2 = patch_size),
             nn.Linear(pixel_values_per_patch, dim),
         )
         
-        self.spatio_temporal_transformer = TVit(
-            dim = dim,
-            depth = depth,
-            heads = heads,
-            dim_head=dim_head,
-            dropout = 0.1,
-            emb_dropout = 0.1
+        self.dropout = nn.Dropout(emb_dropout)
+        
+        pos = RotaryEmbedding(dim = dim_head)
+        
+        modules = []
+        for _ in range(recurrence):
+                modules.append(CrissCrossAttention(pos, max_seq_len))
+                modules.append(nn.Dropout(dropout))
+    
+        self.attn_layers = nn.Sequential(*modules)
+        
+        self.mlp = nn.Linear(dim, dim, bias=False)
+        
+        self.to_pixels = nn.Sequential(
+            nn.Linear(dim, dim),
+            nn.Dropout(emb_dropout),
+            nn.Linear(dim, pixel_values_per_patch)
         )
+     
+    def generate(self, start_tokens, seq_len):
+        _, t, _, h, w = start_tokens.shape
+                
+        out = self.to_embedding(start_tokens)
         
-        self.to_pixels = nn.Linear(dim, pixel_values_per_patch, bias = False)
+        for _ in range(seq_len):
+            x = out[:, -self.max_seq_len:]
             
-    def to_patches(self, x):
-        return self.to_patch_embedding(x)
-
-    def embed(self, x):
-        return self.spatio_temporal_transformer(x)
+            last = self.next_latent(x)[:, -1:]
+            out = torch.cat((out, last), dim = 1)
         
+        out = out[:, t:]
+        
+        return model.recover(out, h, w)
+        
+    def next_latent(self, x_H0):
+        x_H1 = self.attn_layers(x_H0)
+        return self.mlp(x_H1)
+    
     def recover(self, x, h, w):
         p = self.to_pixels(x)
         p = rearrange(p, 'b t p d -> b t (p d)')
         p = rearrange(p, 'b t (d h w) -> b t d h w', h=h, w=w)
         return p
         
-    def forward(self, x):        
+    def forward(self, x):
         h, w = x.shape[3:5]
-        x = self.to_patches(x)
-        x_h = self.embed(x)
+    
+        x_e = self.to_embedding(x)
+        x_h = self.next_latent(x_e)
         
-        x_h_t_p = self.recover(x_h,h,w)
-        x_p = self.recover(x,h,w)
+        x_p = self.recover(x_e,h,w)
+        x_h_p = self.recover(x_h,h,w)
         
-        return x, x_h, x_p, x_h_t_p
+        return x_e, x_h, x_p, x_h_p
